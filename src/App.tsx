7225d074893a4607357c3cb43e78b14b7ea0ae3a
anirudh <anirudh.chauhan.142@gmail.com>
2020-07-17 18:57:01 +0530

    :sparkle: User context added for user authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { UserProvider } from "./contexts/userContext";
 
 function App() {
-	return <div>Fund Impact</div>;
+	return (
+		<UserProvider>
+			<div>Fund Impact</div>
+		</UserProvider>
+	);
 }
 
 export default App;
---
+++ src/contexts/userContext.tsx
@@ -0,0 +1,24 @@
+import React, { createContext } from "react";
+import userReducer from "../reducers/userReducer";
+import { IUserDataContext } from "../models/userProvider";
+
+const defaultUserState: IUserDataContext = {};
+
+const UserDataContext = createContext<IUserDataContext | undefined>(undefined);
+const UserDispatchContext = createContext(userReducer);
+
+interface IUserProviderProps {
+	children: React.ReactNode;
+}
+
+function UserProvider({ children }: IUserProviderProps) {
+	return (
+		<UserDataContext.Provider value={defaultUserState}>
+			<UserDispatchContext.Provider value={userReducer}>
+				{children}
+			</UserDispatchContext.Provider>
+		</UserDataContext.Provider>
+	);
+}
+
+export { UserProvider };
