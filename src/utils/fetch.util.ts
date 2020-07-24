2ce0c073ef4454b3958299905046e8c2f598756a
Shadab Faiz <Shadab.faiz@dhwaniris.com>
2020-07-24 13:29:40 +0530

    Removed the unused codes

--- src/utils/fetch.util.ts
+++
@@ -1,9 +0,0 @@
-/**
- *
- * @description Do not use this for nested objects or array. It is only for simple objects.
- */
-export const convertObjectToQueryParams = (object: { [key: string]: number | string }) => {
-	return Object.keys(object)
-		.map((k) => encodeURIComponent(k) + "=" + encodeURIComponent(object[k]))
-		.join("&");
-};
