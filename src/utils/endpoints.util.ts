57efca3789917d18060098adef96e5287e7542ae
Shadab Faiz <Shadab.faiz@dhwaniris.com>
2020-07-24 17:27:48 +0530

    :wrench: Seperated the api endpoints to separate file

---
+++ src/utils/endpoints.util.ts
@@ -0,0 +1,3 @@
+export const LOGIN_API = `${process.env.REACT_APP_BASEURL}auth/local`;
+export const SIGNUP_API = `${process.env.REACT_APP_BASEURL}auth/local/register`;
+export const ORGANISATION_TYPES_API = `${process.env.REACT_APP_BASEURL}organisation-registration-types`;
