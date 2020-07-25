da57a9151ca8f37683a8f79241234aef4f1fd59f
Shadab Faiz <Shadab.faiz@dhwaniris.com>
2020-07-25 19:40:11 +0530

    :sparkle: Created the table for dashboard

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,8 +1,10 @@
 import React from "react";
-import LandingPage from "../pages/Landing/Landing";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { RouteProps } from "react-router";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
+import DashboardTableContainer from "../components/DashboardTableContainer";
 import { useAuth } from "../contexts/userContext";
+import LandingPage from "../pages/Landing/Landing";
 
 const SignUp = React.lazy(() => import("../components/SignUp"));
 const Login = React.lazy(() => import("../components/Login"));
@@ -19,6 +21,8 @@ function AppRoutes() {
 		<BrowserRouter>
 			<Routes>
 				<PrivateRoute path="dashboard" element={<div>Example Private Route</div>} />
+				<Route path="dashboardTableContainer" element={<DashboardTableContainer />} />
+
 				<Route path="" element={<LandingPage />}>
 					<Route path="login" element={<Login />} />
 					<Route path="signup/:id" element={<SignUp />} />
