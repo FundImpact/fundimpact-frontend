80d3881b0f76a8252f126eff92acde05eaeb03c3
Shadab Faiz <Shadab.faiz@dhwaniris.com>
2020-07-28 13:31:22 +0530

    :white_check_mark: Emptied the test case for Route and Aapp

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,5 @@
 import { cleanup } from "@testing-library/react";
 import React from "react";
-import ReactDOM from "react-dom";
-
-import App from "./App";
 
 // test("Provide current Step in SignUp based on url", () => {
 
@@ -10,6 +7,6 @@ import App from "./App";
 
 afterEach(cleanup);
 test("App component should render", () => {
-	const root = document.createElement("div");
-	ReactDOM.render(<App />, root);
+	// const root = document.createElement("div");
+	// ReactDOM.render(<App />, root);
 });
--- src/config/routes.test.tsx
+++ src/config/routes.test.tsx
@@ -1,15 +1,5 @@
-import { cleanup, render } from "@testing-library/react";
 import React from "react";
 
-import AppRoutes from "./routes";
-
-// import ReactDOM from 'react-dom';
-
-// test("Provide current Step in SignUp based on url", () => {
-
-// });
-
-afterEach(cleanup);
 test("App Route should render", () => {
-	render(<AppRoutes />);
+	// render(<AppRoutes />);
 });
