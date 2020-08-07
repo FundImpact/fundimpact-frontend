638d6274112729034b0ea227a2846ff7bdec76ce
realgcoding <realgcoding@gmail.com>
2020-08-07 12:16:45 +0530

    created a global snackbar component with test cases | removed react-test-renderer

--- src/components/AlertMessage/__test__/AlertMessage.test.tsx
+++ src/components/AlertMessage/__test__/AlertMessage.test.tsx
@@ -1,25 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import AlertMessage from "./../AlertMessage";
-import { render, cleanup } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import renderer from "react-test-renderer";
-
-afterEach(cleanup);
 
 it("renders without crashing", () => {
 	const div = document.createElement("div");
-	ReactDOM.render(<AlertMessage />, div);
+	ReactDOM.render(<AlertMessage msg={"Render Without Crashing"} />, div);
 });
 
-it("renders alert correctly", () => {
+it("renders alert component correctly", () => {
 	const { getByTestId } = render(<AlertMessage severity="success" msg="successfully tested" />);
 	expect(getByTestId("fi-alert")).toHaveTextContent("successfully tested");
-});
-
-it("matches snapshot", () => {
-	const tree = renderer
-		.create(<AlertMessage severity="success" msg="successfully tested" />)
-		.toJSON();
-	expect(tree).toMatchSnapshot();
 });
