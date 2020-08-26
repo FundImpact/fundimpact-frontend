131d77d6c73ff5b5170b128d7c3caf88945b56d3
shubham <shubham.kumar@dhwaniris.com>
2020-08-26 15:40:21 +0530

    table updated

--- src/components/Budget/BudgetLineitem/BudgetLineItem.tsx
+++ src/components/Budget/BudgetLineitem/BudgetLineItem.tsx
@@ -43,357 +43,258 @@ const defaultFormValues: IBudgetTrackingLineitemForm = {
 	budget_targets_project: "",
 	annual_year: "",
 	reporting_date: getTodaysDate(),
-	fy_donor: "",
-	fy_org: "",
-	grant_periods_project: "",
 };
 
-let budgetTargetHash: { [key: string]: { id: string; country: { id: string } } } = {};
+const validate = (values: IBudgetTrackingLineitemForm) => {
+	let errors: Partial<IBudgetTrackingLineitemForm> = {};
+	if (!values.amount) {
+		errors.amount = "Amount is required";
+	}
+	if (!values.note) {
+		errors.note = "Note is required";
+	}
+	if (!values.budget_targets_project) {
+		errors.budget_targets_project = "Budget project is required";
+	}
+	if (!values.annual_year) {
+		errors.annual_year = "Annual year is required";
+	}
+	if (!values.reporting_date) {
+		errors.reporting_date = "Reporting date is required";
+	}
+	return errors;
+};
 
-function BudgetLineItemDialog(props: IBudgetLineitemProps) {
-	// const apolloClient = useApolloClient();
-	// const notificationDispatch = useNotificationDispatch();
-	// const dashboardData = useDashBoardData();
-	// const [selectedDonor, setSelectedDonor] = useState<{
-	// 	id: string;
-	// 	country: { id: string };
-	// } | null>(null);
+function BudgetLineitem(props: IBudgetLineitemProps) {
+	const apolloClient = useApolloClient();
+	const notificationDispatch = useNotificationDispatch();
+	const dashboardData = useDashBoardData();
 
-	// const currentProject = dashboardData?.project;
-	// let initialValues = props.initialValues ? props.initialValues : defaultFormValues;
+	const currentProject = dashboardData?.project;
+	let initialValues = props.initialValues ? props.initialValues : defaultFormValues;
 
-	// const [createProjectBudgetTracking, { loading: creatingLineItem, data }] = useMutation(
-	// 	CREATE_PROJECT_BUDGET_TRACKING
-	// );
-	// const [updateProjectBudgetTracking, { loading: updatingLineItem }] = useMutation(
-	// 	UPDATE_PROJECT_BUDGET_TRACKING
-	// );
-	// const [getBudgetTargetProject] = useLazyQuery(GET_BUDGET_TARGET_PROJECT);
-	// const { data: annualYears } = useQuery(GET_ANNUAL_YEAR_LIST);
+	const [createProjectBudgetTracking, { loading: creatingLineItem }] = useMutation(
+		CREATE_PROJECT_BUDGET_TRACKING
+	);
+	const [updateProjectBudgetTracking, { loading: updatingLineItem }] = useMutation(
+		UPDATE_PROJECT_BUDGET_TRACKING
+	);
+	const [getBudgetTargetProject] = useLazyQuery(GET_BUDGET_TARGET_PROJECT);
+	const { data: annualYears } = useQuery(GET_ANNUAL_YEAR_LIST);
 
-	// const closeDialog = useCallback(() => {
-	// 	createBudgetTrackingLineitemFormSelectFields[2].hidden = false;
-	// 	props.handleClose();
-	// }, []);
+	let oldCachedBudgetTargetProjectData: IGET_BUDGET_TARGET_PROJECT | null = null;
+	try {
+		oldCachedBudgetTargetProjectData = apolloClient.readQuery<IGET_BUDGET_TARGET_PROJECT>({
+			query: GET_BUDGET_TARGET_PROJECT,
+			variables: {
+				filter: {
+					project: currentProject?.id,
+				},
+			},
+		});
+	} catch (error) {}
 
-	// const validate = useCallback(
-	// 	(values: IBudgetTrackingLineitemForm) => {
-	// 		let errors: Partial<IBudgetTrackingLineitemForm> = {};
+	const { data: orgCurrencies } = useQuery(GET_ORG_CURRENCIES_BY_ORG, {
+		variables: {
+			filter: {
+				organization: dashboardData?.organization?.id,
+				isHomeCurrency: true,
+			},
+		},
+	});
 
-	// 		if (values.budget_targets_project) {
-	// 			setSelectedDonor(budgetTargetHash[values.budget_targets_project]);
-	// 			if (
-	// 				budgetTargetHash[values.budget_targets_project].country.id ==
-	// 				dashboardData?.organization?.country?.id
-	// 			) {
-	// 				createBudgetTrackingLineitemFormSelectFields[2].hidden = true;
-	// 			} else {
-	// 				createBudgetTrackingLineitemFormSelectFields[2].hidden = false;
-	// 			}
-	// 		}
+	useEffect(() => {
+		if (!oldCachedBudgetTargetProjectData && currentProject) {
+			getBudgetTargetProject({
+				variables: {
+					filter: {
+						project: currentProject?.id,
+					},
+				},
+			});
+		}
+	}, [oldCachedBudgetTargetProjectData, currentProject, getBudgetTargetProject]);
 
-	// 		if (!values.amount) {
-	// 			errors.amount = "Amount is required";
-	// 		}
-	// 		if (!values.note) {
-	// 			errors.note = "Note is required";
-	// 		}
-	// 		if (!values.budget_targets_project) {
-	// 			errors.budget_targets_project = "Budget project is required";
-	// 		}
-	// 		if (!values.annual_year) {
-	// 			errors.annual_year = "Annual year is required";
-	// 		}
-	// 		if (!values.reporting_date) {
-	// 			errors.reporting_date = "Reporting date is required";
-	// 		}
-	// 		if (!values.grant_periods_project) {
-	// 			errors.grant_periods_project = "Grant period is required";
-	// 		}
-	// 		if (!values.fy_donor && !createBudgetTrackingLineitemFormSelectFields[2].hidden) {
-	// 			errors.fy_donor = "Financial year of donor is required";
-	// 		}
-	// 		if (!values.fy_org) {
-	// 			errors.fy_org = "Financial year of organization is required";
-	// 		}
+	useEffect(() => {
+		if (orgCurrencies?.orgCurrencies?.length) {
+			createBudgetTrackingLineitemForm[1].endAdornment =
+				orgCurrencies.orgCurrencies[0].currency.code;
+		}
+	}, [orgCurrencies]);
 
-	// 		return errors;
-	// 	},
-	// 	[setErrorNotification, dashboardData]
-	// );
+	useEffect(() => {
+		if (oldCachedBudgetTargetProjectData) {
+			createBudgetTrackingLineitemFormSelectFields[0].optionsArray = oldCachedBudgetTargetProjectData.projectBudgetTargets as any;
+		}
+	}, [oldCachedBudgetTargetProjectData]);
 
-	// const { data: grantPeriodProject } = useQuery(GET_GRANT_PERIODS_PROJECT_LIST, {
-	// 	variables: {
-	// 		filter: {
-	// 			project: currentProject?.id,
-	// 		},
-	// 	},
-	// });
+	useEffect(() => {
+		if (annualYears) {
+			createBudgetTrackingLineitemFormSelectFields[1].optionsArray =
+				annualYears.annualYearList;
+		}
+	}, [annualYears]);
 
-	// const { data: financialYearOrg } = useQuery(GET_FINANCIAL_YEARS_ORG_LIST_BY_ORG, {
-	// 	variables: {
-	// 		filter: {
-	// 			organization: dashboardData?.organization?.id,
-	// 		},
-	// 	},
-	// });
+	const onCreate = async (values: IBudgetTrackingLineitemForm) => {
+		const reporting_date = new Date(values.reporting_date);
+		try {
+			await createProjectBudgetTracking({
+				variables: {
+					input: {
+						...values,
+						reporting_date,
+					},
+				},
+				update: (store, { data: { createProjBudgetTracking: lineItemCreated } }) => {
+					try {
+						const data = store.readQuery<IGET_BUDGET_TARCKING_LINE_ITEM>({
+							query: GET_PROJECT_BUDGET_TARCKING,
+							variables: {
+								filter: {
+									budget_targets_project:
+										lineItemCreated.budget_targets_project.id,
+								},
+							},
+						});
+						let budgetLineItems: IBUDGET_TRACKING_LINE_ITEM_RESPONSE[] = data?.projBudgetTrackings
+							? data?.projBudgetTrackings
+							: [];
+						store.writeQuery({
+							query: GET_PROJECT_BUDGET_TARCKING,
+							variables: {
+								filter: {
+									budget_targets_project:
+										lineItemCreated.budget_targets_project.id,
+								},
+							},
+							data: {
+								projBudgetTrackings: [...budgetLineItems, lineItemCreated],
+							},
+						});
+					} catch (err) {}
 
-	// const [getFinancialYearDonor, { data: financialYearDonor }] = useLazyQuery(
-	// 	GET_FINANCIAL_YEARS_DONOR_LIST_BY_DONOR
-	// );
+					try {
+						const amountSpentData = store.readQuery<{
+							projBudgetTrackingsTotalAmount: number;
+						}>({
+							query: GET_PROJECT_BUDGET_TARGET_AMOUNT_SUM,
+							variables: {
+								filter: {
+									budgetTargetsProject: lineItemCreated.budget_targets_project.id,
+								},
+							},
+						});
+						store.writeQuery({
+							query: GET_PROJECT_BUDGET_TARGET_AMOUNT_SUM,
+							variables: {
+								filter: {
+									budgetTargetsProject: lineItemCreated.budget_targets_project.id,
+								},
+							},
+							data: {
+								projBudgetTrackingsTotalAmount:
+									(amountSpentData?.projBudgetTrackingsTotalAmount
+										? amountSpentData?.projBudgetTrackingsTotalAmount
+										: 0) + lineItemCreated.amount,
+							},
+						});
+					} catch (err) {}
+				},
+			});
+			notificationDispatch(
+				setSuccessNotification("Budget Tracking Line Item Creation Success")
+			);
+			props.handleClose();
+		} catch (err) {
+			notificationDispatch(
+				setErrorNotification("Budget Tracking Line Item Creation Failure")
+			);
+			props.handleClose();
+		}
+	};
 
-	// let oldCachedBudgetTargetProjectData: IGET_BUDGET_TARGET_PROJECT | null = null;
-	// try {
-	// 	oldCachedBudgetTargetProjectData = apolloClient.readQuery<IGET_BUDGET_TARGET_PROJECT>({
-	// 		query: GET_BUDGET_TARGET_PROJECT,
-	// 		variables: {
-	// 			filter: {
-	// 				project: currentProject?.id,
-	// 			},
-	// 		},
-	// 	});
-	// } catch (error) {}
-
-	// const { data: orgCurrencies } = useQuery(GET_ORG_CURRENCIES_BY_ORG, {
-	// 	variables: {
-	// 		filter: {
-	// 			organization: dashboardData?.organization?.id,
-	// 			isHomeCurrency: true,
-	// 		},
-	// 	},
-	// });
-
-	// useEffect(() => {
-	// 	if (!oldCachedBudgetTargetProjectData && currentProject) {
-	// 		getBudgetTargetProject({
-	// 			variables: {
-	// 				filter: {
-	// 					project: currentProject?.id,
-	// 				},
-	// 			},
-	// 		});
-	// 	}
-	// }, [oldCachedBudgetTargetProjectData, currentProject, getBudgetTargetProject]);
-
-	// useEffect(() => {
-	// 	if (orgCurrencies && orgCurrencies.orgCurrencies.length) {
-	// 		createBudgetTrackingLineitemForm[1].endAdornment =
-	// 			orgCurrencies.orgCurrencies[0].currency.code;
-	// 	}
-	// }, [orgCurrencies]);
-
-	// useEffect(() => {
-	// 	if (oldCachedBudgetTargetProjectData) {
-	// 		createBudgetTrackingLineitemFormSelectFields[0].optionsArray = oldCachedBudgetTargetProjectData.projectBudgetTargets as any;
-	// 		budgetTargetHash = oldCachedBudgetTargetProjectData.projectBudgetTargets.reduce(
-	// 			(accunulator, current) => {
-	// 				accunulator[current.id] = current.donor;
-	// 				return accunulator;
-	// 			},
-	// 			{} as { [key: string]: { id: string; country: { id: string } } }
-	// 		);
-	// 	}
-	// }, [oldCachedBudgetTargetProjectData]);
-
-	// useEffect(() => {
-	// 	if (annualYears) {
-	// 		createBudgetTrackingLineitemFormSelectFields[1].optionsArray =
-	// 			annualYears.annualYearList;
-	// 	}
-	// }, [annualYears]);
-
-	// useEffect(() => {
-	// 	if (financialYearDonor) {
-	// 		createBudgetTrackingLineitemFormSelectFields[2].optionsArray =
-	// 			financialYearDonor.financialYearsDonorList;
-	// 	}
-	// }, [financialYearDonor]);
-
-	// useEffect(() => {
-	// 	if (financialYearOrg) {
-	// 		createBudgetTrackingLineitemFormSelectFields[3].optionsArray =
-	// 			financialYearOrg.financialYearsOrgList;
-	// 	}
-	// }, [financialYearOrg]);
-
-	// useEffect(() => {
-	// 	if (grantPeriodProject) {
-	// 		createBudgetTrackingLineitemFormSelectFields[4].optionsArray =
-	// 			grantPeriodProject.grantPeriodsProjectList;
-	// 	}
-	// }, [grantPeriodProject]);
-
-	// useEffect(() => {
-	// 	if (selectedDonor) {
-	// 		getFinancialYearDonor({
-	// 			variables: {
-	// 				filter: {
-	// 					donor: selectedDonor.id,
-	// 				},
-	// 			},
-	// 		});
-	// 	}
-	// }, [selectedDonor]);
-
-	// const onCreate = async (values: IBudgetTrackingLineitemForm) => {
-	// 	const reporting_date = new Date(values.reporting_date);
-
-	// 	try {
-	// 		if (createBudgetTrackingLineitemFormSelectFields[2].hidden) {
-	// 			values.fy_donor = values.fy_org;
-	// 		}
-	// 		await createProjectBudgetTracking({
-	// 			variables: {
-	// 				input: {
-	// 					...values,
-	// 					reporting_date,
-	// 				},
-	// 			},
-	// 			update: (store, { data: { createProjBudgetTracking: lineItemCreated } }) => {
-	// 				try {
-	// 					const data = store.readQuery<IGET_BUDGET_TARCKING_LINE_ITEM>({
-	// 						query: GET_PROJECT_BUDGET_TARCKING,
-	// 						variables: {
-	// 							filter: {
-	// 								budget_targets_project:
-	// 									lineItemCreated.budget_targets_project.id,
-	// 							},
-	// 						},
-	// 					});
-	// 					store.writeQuery({
-	// 						query: GET_PROJECT_BUDGET_TARCKING,
-	// 						variables: {
-	// 							filter: {
-	// 								budget_targets_project:
-	// 									lineItemCreated.budget_targets_project.id,
-	// 							},
-	// 						},
-	// 						data: {
-	// 							projBudgetTrackings: [
-	// 								...data!.projBudgetTrackings,
-	// 								lineItemCreated,
-	// 							],
-	// 						},
-	// 					});
-	// 				} catch (err) {}
-
-	// 				try {
-	// 					const amountSpentData = store.readQuery<{
-	// 						projBudgetTrackingsTotalAmount: number;
-	// 					}>({
-	// 						query: GET_PROJECT_BUDGET_TARGET_AMOUNT_SUM,
-	// 						variables: {
-	// 							filter: {
-	// 								budgetTargetsProject: lineItemCreated.budget_targets_project.id,
-	// 							},
-	// 						},
-	// 					});
-	// 					store.writeQuery({
-	// 						query: GET_PROJECT_BUDGET_TARGET_AMOUNT_SUM,
-	// 						variables: {
-	// 							filter: {
-	// 								budgetTargetsProject: lineItemCreated.budget_targets_project.id,
-	// 							},
-	// 						},
-	// 						data: {
-	// 							projBudgetTrackingsTotalAmount:
-	// 								amountSpentData!.projBudgetTrackingsTotalAmount +
-	// 								lineItemCreated.amount,
-	// 						},
-	// 					});
-	// 				} catch (err) {}
-	// 			},
-	// 		});
-	// 		notificationDispatch(
-	// 			setSuccessNotification("Budget Tracking Line Item Creation Success")
-	// 		);
-	// 	} catch (err) {
-	// 		notificationDispatch(
-	// 			setErrorNotification("Budget Tracking Line Item Creation Failure")
-	// 		);
-	// 	} finally {
-	// 		closeDialog();
-	// 	}
-	// };
-
-	// const onUpdate = async (values: IBudgetTrackingLineitemForm) => {
-	// 	try {
-	// 		const reporting_date = new Date(values.reporting_date);
-	// 		if (compareObjectKeys(values, initialValues)) {
-	// 			closeDialog();
-	// 			return;
-	// 		}
-	// 		delete values.id;
-	// 		await updateProjectBudgetTracking({
-	// 			variables: {
-	// 				id: initialValues.id,
-	// 				input: {
-	// 					...values,
-	// 					reporting_date,
-	// 				},
-	// 			},
-	// 			update: async (store, { data: { updateProjBudgetTracking: lineItemCreated } }) => {
-	// 				try {
-	// 					const amountSpentData = store.readQuery<{
-	// 						projBudgetTrackingsTotalAmount: number;
-	// 					}>({
-	// 						query: GET_PROJECT_BUDGET_TARGET_AMOUNT_SUM,
-	// 						variables: {
-	// 							filter: {
-	// 								budgetTargetsProject: lineItemCreated.budget_targets_project.id,
-	// 							},
-	// 						},
-	// 					});
-	// 					const change = lineItemCreated.amount - +initialValues.amount;
-	// 					store.writeQuery({
-	// 						query: GET_PROJECT_BUDGET_TARGET_AMOUNT_SUM,
-	// 						variables: {
-	// 							filter: {
-	// 								budgetTargetsProject: lineItemCreated.budget_targets_project.id,
-	// 							},
-	// 						},
-	// 						data: {
-	// 							projBudgetTrackingsTotalAmount:
-	// 								amountSpentData!.projBudgetTrackingsTotalAmount + change,
-	// 						},
-	// 					});
-	// 				} catch (err) {}
-	// 			},
-	// 		});
-	// 		notificationDispatch(
-	// 			setSuccessNotification("Budget Tracking Line Item Updation Success")
-	// 		);
-	// 	} catch (err) {
-	// 		notificationDispatch(
-	// 			setErrorNotification("Budget Tracking Line Item Updation Failure")
-	// 		);
-	// 	} finally {
-	// 		closeDialog();
-	// 	}
-	// };
+	const onUpdate = async (values: IBudgetTrackingLineitemForm) => {
+		try {
+			const reporting_date = new Date(values.reporting_date);
+			if (compareObjectKeys(values, initialValues)) {
+				props.handleClose();
+				return;
+			}
+			delete values.id;
+			await updateProjectBudgetTracking({
+				variables: {
+					id: initialValues.id,
+					input: {
+						...values,
+						reporting_date,
+					},
+				},
+				update: async (store, { data: { updateProjBudgetTracking: lineItemCreated } }) => {
+					try {
+						const amountSpentData = store.readQuery<{
+							projBudgetTrackingsTotalAmount: number;
+						}>({
+							query: GET_PROJECT_BUDGET_TARGET_AMOUNT_SUM,
+							variables: {
+								filter: {
+									budgetTargetsProject: lineItemCreated.budget_targets_project.id,
+								},
+							},
+						});
+						const change = lineItemCreated.amount - +initialValues.amount;
+						store.writeQuery({
+							query: GET_PROJECT_BUDGET_TARGET_AMOUNT_SUM,
+							variables: {
+								filter: {
+									budgetTargetsProject: lineItemCreated.budget_targets_project.id,
+								},
+							},
+							data: {
+								projBudgetTrackingsTotalAmount:
+									(amountSpentData?.projBudgetTrackingsTotalAmount
+										? amountSpentData?.projBudgetTrackingsTotalAmount
+										: 0) + change,
+							},
+						});
+					} catch (err) {
+						console.log("err :>> ", err);
+						// throw err;
+					}
+				},
+			});
+			notificationDispatch(
+				setSuccessNotification("Budget Tracking Line Item Updation Success")
+			);
+			props.handleClose();
+		} catch (err) {
+			notificationDispatch(
+				setErrorNotification("Budget Tracking Line Item Updation Failure")
+			);
+			props.handleClose();
+		}
+	};
 	return (
-		// <FormDialog
-		// 	handleClose={closeDialog}
-		// 	open={props.open}
-		// 	loading={creatingLineItem || updatingLineItem}
-		// 	title="Report Expenditure"
-		// 	subtitle="Physical addresses of your organizatin like headquater, branch etc."
-		// 	workspace={dashboardData?.workspace?.name}
-		// 	project={dashboardData?.project?.name ? dashboardData?.project?.name : ""}
-		// >
-		// 	<CommonForm
-		// 		initialValues={initialValues}
-		// 		validate={validate}
-		// 		onSubmit={onCreate}
-		// 		onCancel={props.handleClose}
-		// 		inputFields={createBudgetTrackingLineitemForm}
-		// 		selectFields={createBudgetTrackingLineitemFormSelectFields}
-		// 		formAction={props.formAction}
-		// 		onUpdate={onUpdate}
-		// 	/>
-		// </FormDialog>
-		<div></div>
+		<FormDialog
+			handleClose={props.handleClose}
+			open={props.open}
+			loading={creatingLineItem || updatingLineItem}
+			title="Report Expenditure"
+			subtitle="Physical addresses of your organizatin like headquater, branch etc."
+			workspace={dashboardData?.workspace?.name}
+			project={dashboardData?.project?.name ? dashboardData?.project?.name : ""}
+		>
+			<CommonForm
+				initialValues={initialValues}
+				validate={validate}
+				onSubmit={onCreate}
+				onCancel={props.handleClose}
+				inputFields={createBudgetTrackingLineitemForm}
+				selectFields={createBudgetTrackingLineitemFormSelectFields}
+				formAction={props.formAction}
+				onUpdate={onUpdate}
+			/>
+		</FormDialog>
 	);
 }
 
-export default BudgetLineItemDialog;
+export default React.memo(BudgetLineitem);
