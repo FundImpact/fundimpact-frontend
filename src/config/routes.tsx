2caac826f166b434b90e615ff229a1d103676d9e
Shadab Faiz <Shadab.faiz@dhwaniris.com>
2020-08-28 20:32:47 +0530

    :boom: REfactored the routing part

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,15 +1,15 @@
-import { ApolloProvider } from '@apollo/client';
-import React from 'react';
-import { RouteProps } from 'react-router';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { ApolloProvider } from "@apollo/client";
+import React from "react";
+import { RouteProps } from "react-router";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
-import DashboardTableContainer from '../components/Dasboard/Table/DashboardTableContainer';
-import { DashboardProvider } from '../contexts/dashboardContext';
-import { NotificationProvider } from '../contexts/notificationContext';
-import { useAuth } from '../contexts/userContext';
-import LandingPage from '../pages/Landing/Landing';
-import SettingsContainer from '../pages/settings/settings';
-import { client } from './grapql';
+import DashboardTableContainer from "../components/Dasboard/Table/DashboardTableContainer";
+import { DashboardProvider } from "../contexts/dashboardContext";
+import { NotificationProvider } from "../contexts/notificationContext";
+import { useAuth } from "../contexts/userContext";
+import LandingPage from "../pages/Landing/Landing";
+import SettingsContainer from "../pages/settings/settings";
+import { client } from "./grapql";
 
 const SignUp = React.lazy(() => import("../pages/Signup/SignUp"));
 const Login = React.lazy(() => import("../pages/Login/Login"));
@@ -25,41 +25,33 @@ function PrivateRoute({ children, ...rest }: RouteProps): React.ReactElement | n
 function AppRoutes() {
 	return (
 		<BrowserRouter>
-			<Routes>
-				<PrivateRoute
-					path="dashboard"
-					element={
-						<ApolloProvider client={client}>
-							<NotificationProvider>
-								<DashboardProvider>
+			<ApolloProvider client={client}>
+				<NotificationProvider>
+					<DashboardProvider>
+						<Routes>
+							<PrivateRoute
+								path="dashboard"
+								element={
 									<DashboardContainer
 										left={null}
 										main={<DashboardTableContainer />}
 									/>
-								</DashboardProvider>
-							</NotificationProvider>
-						</ApolloProvider>
-					}
-				/>
-				<PrivateRoute
-					path="settings/*"
-					element={
-						<ApolloProvider client={client}>
-							<NotificationProvider>
-								<DashboardProvider>
-									<SettingsContainer></SettingsContainer>
-								</DashboardProvider>
-							</NotificationProvider>
-						</ApolloProvider>
-					}
-				/>
+								}
+							/>
+							<PrivateRoute
+								path="settings/*"
+								element={<SettingsContainer></SettingsContainer>}
+							/>
 
-				<Route path="" element={<LandingPage />}>
-					<Route path="login" element={<Login />} />
-					<Route path="signup/:id" element={<SignUp />} />
-					<Route path="signup" element={<SignUp />} />
-				</Route>
-			</Routes>
+							<Route path="" element={<LandingPage />}>
+								<Route path="login" element={<Login />} />
+								<Route path="signup/:id" element={<SignUp />} />
+								<Route path="signup" element={<SignUp />} />
+							</Route>
+						</Routes>
+					</DashboardProvider>
+				</NotificationProvider>
+			</ApolloProvider>
 		</BrowserRouter>
 	);
 }
--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,8 +1,8 @@
 import "./index.css";
 
-import { Box, Grid, Typography } from "@material-ui/core";
+import { Box, Button, Grid, Typography } from "@material-ui/core";
 import React from "react";
-import { Link, matchPath, useLocation } from "react-router-dom";
+import { matchPath, Outlet, useLocation, useNavigate } from "react-router-dom";
 
 import useRouteResolver from "../../hooks/routes/useRouteResolver";
 import useSignUpStep from "../../hooks/signup/useSignupStep";
@@ -43,7 +43,7 @@ function ActionDescription({ stepNumber }: { stepNumber: number | undefined }) {
 }
 
 function LandingPage() {
-	// const navigate = useNavigate();
+	const navigate = useNavigate();
 	const { pathname } = useLocation();
 	const loginPathMatch = matchPath("login", pathname);
 	const location = useLocation();
@@ -72,24 +72,21 @@ function LandingPage() {
 							</Box>
 						</Typography>
 						{loginPathMatch ? (
-							<Link to="/signup">Login</Link>
+							<Button
+								onClick={() => navigate("/signup")}
+								variant={"contained"}
+								color="secondary"
+							>
+								Sign Up
+							</Button>
 						) : (
-							// <Button
-							// 	onClick={() => navigate("/signup")}
-							// 	variant={"contained"}
-							// 	color="secondary"
-							// >
-							// 	Sign Up
-							// </Button>
-							<Link to="/login">Login</Link>
-
-							// <Button
-							// 	onClick={() => navigate("/login")}
-							// 	variant={"contained"}
-							// 	color="secondary"
-							// >
-							// 	Login
-							// </Button>
+							<Button
+								onClick={() => navigate("/login")}
+								variant={"contained"}
+								color="secondary"
+							>
+								Login
+							</Button>
 						)}
 					</Box>
 				</Box>
@@ -104,7 +101,7 @@ function LandingPage() {
 						<img src={require("../../assets/images/logo.svg")} alt="" />
 					</Grid>
 					<Grid item xs={12} style={{ display: "flex" }}>
-						{/* <Outlet /> */}
+						<Outlet />
 					</Grid>
 				</Grid>
 			</Grid>
