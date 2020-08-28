735c578bad17fb9b0dad84b805eb416620608f07
Shadab Faiz <Shadab.faiz@dhwaniris.com>
2020-08-28 21:01:06 +0530

    Removed the unnecessary Dashboard Prodiver on setting level

--- src/pages/settings/settings.tsx
+++ src/pages/settings/settings.tsx
@@ -5,7 +5,6 @@ import { Route, Routes } from "react-router-dom";
 import { sidePanelStyles } from "../../components/Dasboard/styles";
 import LeftPanel from "../../components/LeftPanel/LeftPanel";
 import Snackbar from "../../components/Snackbar/Snackbar";
-import { DashboardProvider } from "../../contexts/dashboardContext";
 import { useNotificationData } from "../../contexts/notificationContext";
 import IDefaultView from "./defaultView";
 import { DonorContainer } from "./donor/container";
@@ -16,41 +15,39 @@ export default function SettingContainer() {
 	const notificationData = useNotificationData();
 
 	return (
-		<DashboardProvider>
-			<Container
-				disableGutters
-				container
-				className={classes.root}
-				maxWidth={"xl"}
-				component={Grid}
-			>
-				<Grid container>
-					<Grid item xs={12} md={3}>
-						<Box position="sticky" top={0} left={0} style={{ width: "100%" }}>
-							<Grid container>
-								<Grid item xs={2}>
-									<LeftPanel />
-								</Grid>
-								<Grid item xs={10}>
-									<SettingsSidebar></SettingsSidebar>
-								</Grid>
+		<Container
+			disableGutters
+			container
+			className={classes.root}
+			maxWidth={"xl"}
+			component={Grid}
+		>
+			<Grid container>
+				<Grid item xs={12} md={3}>
+					<Box position="sticky" top={0} left={0} style={{ width: "100%" }}>
+						<Grid container>
+							<Grid item xs={2}>
+								<LeftPanel />
 							</Grid>
-						</Box>
-					</Grid>
-					<Grid item xs={12} md={9}>
-						<Routes>
-							<Route path="donors" element={<DonorContainer />} />
-							<Route path="/" element={<IDefaultView />} />
-						</Routes>
-					</Grid>
+							<Grid item xs={10}>
+								<SettingsSidebar></SettingsSidebar>
+							</Grid>
+						</Grid>
+					</Box>
 				</Grid>
-				{notificationData!.successNotification && (
-					<Snackbar severity="success" msg={notificationData!.successNotification} />
-				)}
-				{notificationData!.errorNotification && (
-					<Snackbar severity="error" msg={notificationData!.errorNotification} />
-				)}
-			</Container>
-		</DashboardProvider>
+				<Grid item xs={12} md={9}>
+					<Routes>
+						<Route path="donors" element={<DonorContainer />} />
+						<Route path="/" element={<IDefaultView />} />
+					</Routes>
+				</Grid>
+			</Grid>
+			{notificationData!.successNotification && (
+				<Snackbar severity="success" msg={notificationData!.successNotification} />
+			)}
+			{notificationData!.errorNotification && (
+				<Snackbar severity="error" msg={notificationData!.errorNotification} />
+			)}
+		</Container>
 	);
 }
