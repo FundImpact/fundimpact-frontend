e18146fb8297529f783fad6c1d116cec7c710524
realgcoding <realgcoding@gmail.com>
2020-08-30 16:02:33 +0530

    removed code smells

--- src/components/Forms/FYDonorYearTagsForm/FYDonorYearTags.tsx
+++ src/components/Forms/FYDonorYearTagsForm/FYDonorYearTags.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useMutation, useQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client";
 import { Formik, Form } from "formik";
 import {
 	Grid,
