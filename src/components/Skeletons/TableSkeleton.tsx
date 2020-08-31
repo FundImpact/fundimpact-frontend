7dde687938ebce0839f86dff0d02fe798cd948d4
shubhamsingh3211 <69340232+shubhamsingh3211@users.noreply.github.com>
2020-08-31 16:37:49 +0530

    table skeleton created (#98)
    Co-authored-by: Shadab Faiz <ShadabFaiz@users.noreply.github.com>

---
+++ src/components/Skeletons/TableSkeleton.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Skeleton from "@material-ui/lab/Skeleton";
+import { ListItem, List, ListItemText } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+	root: {
+		paddingTop: "1px",
+		paddingBottom: "1px",
+	},
+});
+
+function TableSkeleton({ lines = 6 }: { lines?: number }) {
+	const classes = useStyles();
+	return (
+		<div>
+			<List component="nav" aria-label="main mailbox folders">
+				<ListItem className={classes.root}>
+					<ListItemText>
+						<Skeleton
+							variant="rect"
+							width="100%"
+							height={40}
+							animation="wave"
+						></Skeleton>
+					</ListItemText>
+				</ListItem>
+
+				{Array.from({ length: lines }).map((element, index) => (
+					<ListItem className={classes.root} key={index}>
+						<ListItemText>
+							<Skeleton variant="text" animation="wave"></Skeleton>
+						</ListItemText>
+					</ListItem>
+				))}
+			</List>
+		</div>
+	);
+}
+
+export default TableSkeleton;
