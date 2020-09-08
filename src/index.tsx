e4557115474be8a551baa1d2c1749f27f0e58339
Shadab Faiz <shadab.faiz@dhwaniris.com>
2020-09-08 17:55:16 +0530

    :sparkle: Implemented basic level of react-intl

--- src/index.tsx
+++ src/index.tsx
@@ -2,31 +2,70 @@ import "./index.css";
 
 import React from "react";
 import ReactDOM from "react-dom";
+import { IntlProvider } from "react-intl";
 
 import App from "./App";
 import { UIProvider } from "./contexts/uiContext";
 import { UserProvider } from "./contexts/userContext";
 import * as serviceWorker from "./serviceWorker";
 
+// function loadLocaleData(locale: string) {
+// 	switch (locale) {
+// 		case "fr":
+// 			return import("../src/compiled-lang/en.json");
+// 		default:
+// 			return import("../src/compiled-lang/en.json");
+// 	}
+// }
+
+// function App(props) {
+// 	return (
+// 		<IntlProvider locale={props.locale} defaultLocale="en" messages={props.messages}>
+// 		</IntlProvider>
+// 	);
+// }
+
+// async function bootstrapApplication(locale, mainDiv) {
+// 	const messages = await loadLocaleData(locale);
+// 	ReactDOM.render(<App locale={locale} messages={messages} />, mainDiv);
+// }
+
 function loadLocaleData(locale: string) {
-	switch (
-		locale
-		// case 'fr':
-		//   return import('compiled-lang/fr.json')
-		// default:
-		//   return import('compiled-lang/en.json')
-	) {
+	const defaultLocal = import(`../src/compiled-lang/en.json`);
+	if (!locale) return defaultLocal;
+	switch (locale) {
+		case "hi":
+			return import(`../src/compiled-lang/hi.json`);
+		default:
+			return defaultLocal;
 	}
+
+	/**
+	 * @description The code below seems smaller than the one above for loading different
+	 * the language's file, but we are still not using it because, in the apporach mention below,
+	 * we cannot get types on the compiled lang.
+	 */
+	// try {
+	// 	return import(`../src/compiled-lang/${locale}.json`);
+	// } catch (error) {
+	// 	console.error(
+	// 		`Failed to fetch lang data for locale: ${locale}. Switching to default locale`
+	// 	);
+	// 	return defaultLocal;
+	// }
 }
 
-(function () {
+(async function () {
 	const locale = navigator.languages[0] || navigator.language;
+	const messages = await loadLocaleData("hi");
 	ReactDOM.render(
-		<UIProvider>
-			<UserProvider>
-				<App />
-			</UserProvider>
-		</UIProvider>,
+		<IntlProvider messages={messages.default} locale={locale} defaultLocale="en">
+			<UIProvider>
+				<UserProvider>
+					<App />
+				</UserProvider>
+			</UIProvider>
+		</IntlProvider>,
 		document.getElementById("root")
 	);
 })();
