539c2f151e6a1b829776239e0906a532ce948519
Shadab Faiz <shadab.faiz@dhwaniris.com>
2020-09-17 17:14:18 +0530

    :sparkle: Added Deliverables modules

--- src/utils/access/modules.list.ts
+++ src/utils/access/modules.list.ts
@@ -1,14 +1,16 @@
 import { BUDGET_MODULE } from "./modules/budget/budget.module";
+import { DELIVERABLES_MODULE } from "./modules/deliverables/deliverable.module";
 import { IMPACT_MODULE } from "./modules/impact/impact.module";
 
 export enum MODULE_CODES {
 	BUDGET = "budget",
 	IMPACT = "impact",
-	// DELIVERABLES = "deliverables",
+	DELIVERABLES = "deliverables",
 	// GRANT_PERIOD = "grant_period",
 }
 
 export const MODULES = {
 	[MODULE_CODES.BUDGET]: BUDGET_MODULE,
 	[MODULE_CODES.IMPACT]: IMPACT_MODULE,
+	[MODULE_CODES.DELIVERABLES]: DELIVERABLES_MODULE,
 } as const;
