0e344cefd4e917120828520ce763c379d1993f83
realgcoding <realgcoding@gmail.com>
2020-09-18 18:21:51 +0530

    test cases updated

--- src/utils/commonFormattedMessage.tsx
+++ src/utils/commonFormattedMessage.tsx
@@ -6,25 +6,22 @@ import React, { useState } from "react";
 
 function CommonFormTitleFormattedMessage(formAction: any) {
 	const intl = useIntl();
-	const [newOrEdit, setNewOrEdit] = useState<React.ReactNode>();
+	// const [newOrEdit, setNewOrEdit] = useState<React.ReactNode>();
+	let newOrEdit;
 	if (
 		formAction === (FORM_ACTIONS.UPDATE || DELIVERABLE_ACTIONS.UPDATE || IMPACT_ACTIONS.UPDATE)
 	) {
-		setNewOrEdit(
-			intl.formatMessage({
-				id: "editFormHeading",
-				defaultMessage: "Edit",
-				description: `This text will be show on forms for Edit`,
-			})
-		);
+		newOrEdit = intl.formatMessage({
+			id: "editFormHeading",
+			defaultMessage: "Edit",
+			description: `This text will be show on forms for Edit`,
+		});
 	} else {
-		setNewOrEdit(
-			intl.formatMessage({
-				id: "newFormHeading",
-				defaultMessage: "New",
-				description: `This text will be show on forms for New`,
-			})
-		);
+		newOrEdit = intl.formatMessage({
+			id: "newFormHeading",
+			defaultMessage: "New",
+			description: `This text will be show on forms for New`,
+		});
 	}
 	return {
 		newOrEdit,
