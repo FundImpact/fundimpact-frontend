96677a0e898127fe0f7a103b0023424aa37e66ad
shubham <shubham.kumar@dhwaniris.com>
2020-09-28 11:04:53 +0530

    add role basic setup done

---
+++ src/components/Forms/AddRoleForm/AddRoleFormView.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+import CommonForm from "../CommonForm/CommonForm";
+
+function AddRoleView() {
+	return (
+		<Grid container>
+			<Grid item xs={12}>
+				{/* <CommonForm  /> */}
+			</Grid>
+		</Grid>
+	);
+}
+
+export default AddRoleView;
