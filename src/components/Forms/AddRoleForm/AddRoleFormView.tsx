d806d923fcfc566831f6e1479b46f3e016eea0ba
shubhamsingh3211 <69340232+shubhamsingh3211@users.noreply.github.com>
2020-09-30 15:53:56 +0530

    Add role form created #2 (#175)
    * add role basic setup done
    
    * add role form created
    
    * add role basic setup done (#174)
    
    * Sdgcard (#181)
    
    * implemented UI for invite user , invited user table and user details update
    
    * integrated invite user form and invite user table with backend
    
    * implemented invited user login with jwt in url , removed dates from cards , implemented pagination and sorting to invited user table
    
    * implemented sorting and pagination on invited user table and integrated send invitation to user with backend , implemented sdg card on project dashboard
    
    * duplication removed
    
    * CI error removed
    
    Co-authored-by: Shadab Faiz <ShadabFaiz@users.noreply.github.com>
    
    * add role form created
    
    Co-authored-by: Rahul Chawla <63209653+realgcoding@users.noreply.github.com>
    Co-authored-by: Shadab Faiz <ShadabFaiz@users.noreply.github.com>

--- src/components/Forms/AddRoleForm/AddRoleFormView.tsx
+++ src/components/Forms/AddRoleForm/AddRoleFormView.tsx
@@ -1,13 +1,55 @@
 import React from "react";
-import { Grid } from "@material-ui/core";
-import CommonForm from "../CommonForm/CommonForm";
+import { Grid, Box, CircularProgress } from "@material-ui/core";
+import CommonForm from "../../CommonForm";
+import { IAddRole } from "../../../models/AddRole";
+import { addRoleForm } from "./inputField.json";
+import { FORM_ACTIONS } from "../../../models/constants";
+import { FormikState } from "formik";
 
-function AddRoleView() {
+function AddRoleView({
+	getInitialValues,
+	validate,
+	onCreate,
+	roleCreationLoading,
+}: {
+	getInitialValues: () => IAddRole;
+	validate: (values: IAddRole) => Partial<IAddRole>;
+	onCreate: (
+		valuesSubmitted: IAddRole,
+		{
+			resetForm,
+		}: {
+			resetForm?: (nextState?: Partial<FormikState<any>> | undefined) => void;
+		}
+	) => Promise<void>;
+	roleCreationLoading: boolean;
+}) {
+	const initialValues = getInitialValues();
+
 	return (
 		<Grid container>
 			<Grid item xs={12}>
-				{/* <CommonForm  /> */}
+				<CommonForm
+					onCreate={onCreate}
+					onUpdate={() => {}}
+					formAction={FORM_ACTIONS.CREATE}
+					initialValues={initialValues}
+					inputFields={addRoleForm}
+					validate={validate}
+					cancelButtonName="Reset"
+					createButtonName="Add"
+				/>
 			</Grid>
+			{roleCreationLoading && (
+				<Box
+					position="fixed"
+					left="50%"
+					top="50%"
+					style={{ transform: "translate(-50%, -50%)" }}
+				>
+					<CircularProgress />
+				</Box>
+			)}
 		</Grid>
 	);
 }
