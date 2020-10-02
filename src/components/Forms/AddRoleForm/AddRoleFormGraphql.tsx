243c1952c44f38b39dd0441bafd1863ad299f51d
shubham <shubham.kumar@dhwaniris.com>
2020-10-03 02:52:46 +0530

    role table craeted

--- src/components/Forms/AddRoleForm/AddRoleFormGraphql.tsx
+++ src/components/Forms/AddRoleForm/AddRoleFormGraphql.tsx
@@ -1,22 +1,50 @@
 import React, { useState, useEffect } from "react";
 import AddRoleFormContainer from "./AddRoleFormContainer";
-import { useMutation } from "@apollo/client";
+import { useMutation, useLazyQuery } from "@apollo/client";
 import { CREATE_ORGANIZATION_USER_ROLE } from "../../../graphql/AddRole/mutation";
 import {
 	ICreateOrganizationUserRole,
 	ICreateOrganizationUserRoleVariables,
 } from "../../../models/AddRole/mutation";
+import { useLocation } from "react-router-dom";
+import { FORM_ACTIONS } from "../../../models/constants";
+import { IGetUserRole } from "../../../models/access/query";
+import { GET_USER_ROLES } from "../../../graphql/User/query";
+import { useAuth } from "../../../contexts/userContext";
 
 function AddRoleFormGraphql() {
 	const [createOrganizationUserRole, { loading }] = useMutation<
 		ICreateOrganizationUserRole,
 		ICreateOrganizationUserRoleVariables
-	>(CREATE_ORGANIZATION_USER_ROLE)
+	>(CREATE_ORGANIZATION_USER_ROLE);
+
+	const user = useAuth();
+	const location = useLocation();
+
+	const [getUserRoles, { data: userRoleData }] = useLazyQuery<IGetUserRole>(GET_USER_ROLES);
+
+	useEffect(() => {
+		if (user) {
+			getUserRoles({
+				variables: {
+					filter: {
+						role: (location?.state as { role: string })?.role || user.user?.role?.id,
+					},
+				},
+			});
+		}
+	}, [user, getUserRoles]);
 
 	return (
 		<AddRoleFormContainer
 			roleCreationLoading={loading}
 			createOrganizationUserRole={createOrganizationUserRole}
+			userRoleData={userRoleData}
+			formType={
+				(location?.state as { role: string })?.role
+					? FORM_ACTIONS.UPDATE
+					: FORM_ACTIONS.CREATE
+			}
 		/>
 	);
 }
--- src/pages/settings/UserRole/container.tsx
+++ src/pages/settings/UserRole/container.tsx
@@ -1,43 +1,62 @@
 import React from "react";
 import UserRoleForm from "../../../components/Forms/UserRole";
-import { Box, Grid, Paper, Typography } from "@material-ui/core";
+import { Box, Grid, Paper, Typography, Fab } from "@material-ui/core";
 import { FORM_ACTIONS } from "../../../models/constants";
 import { FormattedMessage } from "react-intl";
 import InvitedUserTable from "../../../components/Table/InvitedUser";
-export const UserRoleContainer = () => {
-	return (
-		<Box>
-			<Box m={1}>
-				<Typography variant="h6">
-					<FormattedMessage
-						id={`userRoleHeading`}
-						defaultMessage={`User Roles`}
-						description={`This text will be shown on Setting page for user role heading`}
-					/>
-				</Typography>
-			</Box>
+import RoleTable from "../../../components/Table/RoleTable";
+import AddButton from "../../../components/Dasboard/AddButton";
+import AddIcon from "@material-ui/icons/Add";
+import { Link } from "react-router-dom";
+import { useAuth } from "../../../contexts/userContext";
 
-			<Grid item md={12}>
-				<Paper style={{ height: "250px" }}>
-					<Box m={1} p={3}>
-						<UserRoleForm type={FORM_ACTIONS.CREATE} />
-					</Box>
-				</Paper>
-			</Grid>
-			<Grid md={12}>
-				<Box m={1} mb={0}>
+export const UserRoleContainer = () => {
+	const user = useAuth();
+
+	return (
+		<Box p={2}>
+			<Grid container spacing={2}>
+				<Grid item xs={12}>
 					<Typography variant="h6">
 						<FormattedMessage
-							id={`invitedUserHeading`}
-							defaultMessage={`Invited Users`}
-							description={`This text will be shown on Setting page for invited user heading on role tab`}
+							id={`userRoleHeading`}
+							defaultMessage={`User Roles`}
+							description={`This text will be shown on Setting page for user role heading`}
 						/>
 					</Typography>
-				</Box>
-				<Box>
-					<InvitedUserTable />
-				</Box>
+				</Grid>
+
+				<Grid item xs={12}>
+					<Paper style={{ height: "250px" }}>
+						<Box p={2}>
+							<UserRoleForm type={FORM_ACTIONS.CREATE} />
+						</Box>
+					</Paper>
+				</Grid>
+				<Grid item xs={12}>
+					<Typography variant="h6">
+						<FormattedMessage
+							id={`rolesHeading`}
+							defaultMessage={`Roles`}
+							description={`This text will be shown on Setting page as heading og roles table`}
+						/>
+					</Typography>
+				</Grid>
+				<Grid item xs={12}>
+					<RoleTable />
+				</Grid>
 			</Grid>
+			<Link to={{ pathname: "/settings/add_role" }}>
+				<Fab
+					style={{ position: "fixed", right: "10px", bottom: "10px" }}
+					data-testid="add-role-button"
+					color="primary"
+					aria-label="add"
+					disableRipple
+				>
+					<AddIcon />
+				</Fab>
+			</Link>
 		</Box>
 	);
 };
