c498c33296a6b929c90d83a3f772ba3237ea6d4f
shubham <shubham.kumar@dhwaniris.com>
2020-10-04 09:51:50 +0530

    new user roles mocks added

--- src/components/Table/ImpactCategoryTable/__test__/impactCategoryTable.test.tsx
+++ src/components/Table/ImpactCategoryTable/__test__/impactCategoryTable.test.tsx
@@ -179,11 +179,13 @@ const mocks = [
 	budgetTragetAmountSum,
 	{
     request: {
-      query: GET_USER_ROLES,
-      variables: {
-        id: "1",
-      },
-    },
+		query: GET_USER_ROLES,
+		variables: {
+			filter:{
+				role: "1",
+			}
+		},
+	},
     result: { data: mockUserRoles },
   },
 	{
