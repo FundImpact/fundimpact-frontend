d6a03b127c5998f3ad64777aded4e4ad2573dbf4
shubham <shubham.kumar@dhwaniris.com>
2020-10-04 15:31:59 +0530

    Revert "netlify bug fix 2"
    This reverts commit 58f1d54ccd9f97d93ec1dbc0c71f6f6e19858548.

--- src/components/Table/RoleTable/RoleTableView.tsx
+++ src/components/Table/RoleTable/RoleTableView.tsx
@@ -64,121 +64,119 @@ function RoleTableView({
 		);
 	}
 
-	return <div>Hello</div>;
+	const tableStyles = styledTable();
+	let tableHeader = filterTableHeadingAccordingToUserAccess(userRoleEditAccess);
 
-	// const tableStyles = styledTable();
-	// let tableHeader = filterTableHeadingAccordingToUserAccess(userRoleEditAccess);
-
-	// return (
-	// 	<Grid container justify="center">
-	// 		<Grid item xs={7}>
-	// 			<TableContainer component={Paper}>
-	// 				<Table aria-label="simple table">
-	// 					<TableHead>
-	// 						<TableRow color="primary">
-	// 							{tableHeader.map(
-	// 								(
-	// 									heading: { label: string; keyMapping?: string },
-	// 									index: number
-	// 								) => (
-	// 									<TableCell
-	// 										className={tableStyles.th}
-	// 										key={index}
-	// 										align="left"
-	// 									>
-	// 										{heading.label}
-	// 										{/* {heading.keyMapping && (
-	// 											<TableSortLabel
-	// 												active={orderBy === heading.keyMapping}
-	// 												onClick={() => {
-	// 													if (orderBy === heading.keyMapping) {
-	// 														setOrder &&
-	// 															setOrder(
-	// 																order === "asc" ? "desc" : "asc"
-	// 															);
-	// 													} else {
-	// 														setOrderBy &&
-	// 															setOrderBy(
-	// 																heading.keyMapping || ""
-	// 															);
-	// 													}
-	// 												}}
-	// 												direction={order}
-	// 											></TableSortLabel>
-	// 										)} */}
-	// 									</TableCell>
-	// 								)
-	// 							)}
-	// 						</TableRow>
-	// 					</TableHead>
-	// 					<TableBody>
-	// 						{userRoles.map(
-	// 							(
-	// 								role: { id: string; name: string; type: string },
-	// 								index: number
-	// 							) => (
-	// 								<TableRow key={role.id}>
-	// 									<TableCell align="left">{index + 1}</TableCell>
-	// 									{keyNames.map((keyName: string, i: number) => {
-	// 										return (
-	// 											<TableCell key={i} align="left">
-	// 												{getValueFromObject(role, keyName.split(","))}
-	// 											</TableCell>
-	// 										);
-	// 									})}
-	// 									{userRoleEditAccess && (
-	// 										<TableCell align="left">
-	// 											<Button
-	// 												variant="contained"
-	// 												size="small"
-	// 												color="primary"
-	// 												component={Link}
-	// 												to="/settings/add_role"
-	// 												state={{ role: role.id }}
-	// 											>
-	// 												Edit Role
-	// 											</Button>
-	// 										</TableCell>
-	// 									)}
-	// 								</TableRow>
-	// 							)
-	// 						)}
-	// 					</TableBody>
-	// 					{
-	// 						// <TableFooter>
-	// 						// 	<TableRow>
-	// 						// 		<TablePagination
-	// 						// 			rowsPerPageOptions={[]}
-	// 						// 			colSpan={8}
-	// 						// 			count={count}
-	// 						// 			rowsPerPage={count > 10 ? 10 : count}
-	// 						// 			page={page}
-	// 						// 			SelectProps={{
-	// 						// 				inputProps: { "aria-label": "rows per page" },
-	// 						// 				native: true,
-	// 						// 			}}
-	// 						// 			onChangePage={(
-	// 						// 				event: React.MouseEvent<HTMLButtonElement> | null,
-	// 						// 				newPage: number
-	// 						// 			) => {
-	// 						// 				if (newPage > page) {
-	// 						// 					changePage();
-	// 						// 				} else {
-	// 						// 					changePage(true);
-	// 						// 				}
-	// 						// 				setPage(newPage);
-	// 						// 			}}
-	// 						// 			onChangeRowsPerPage={() => {}}
-	// 						// 			style={{ paddingRight: "40px" }}
-	// 						// 		/>
-	// 						// 	</TableRow>
-	// 						// </TableFooter>
-	// 					}
-	// 				</Table>
-	// 			</TableContainer>
-	// 		</Grid>
-	// 	</Grid>
-	// );
+	return (
+		<Grid container justify="center">
+			<Grid item xs={7}>
+				<TableContainer component={Paper}>
+					<Table aria-label="simple table">
+						<TableHead>
+							<TableRow color="primary">
+								{tableHeader.map(
+									(
+										heading: { label: string; keyMapping?: string },
+										index: number
+									) => (
+										<TableCell
+											className={tableStyles.th}
+											key={index}
+											align="left"
+										>
+											{heading.label}
+											{/* {heading.keyMapping && (
+												<TableSortLabel
+													active={orderBy === heading.keyMapping}
+													onClick={() => {
+														if (orderBy === heading.keyMapping) {
+															setOrder &&
+																setOrder(
+																	order === "asc" ? "desc" : "asc"
+																);
+														} else {
+															setOrderBy &&
+																setOrderBy(
+																	heading.keyMapping || ""
+																);
+														}
+													}}
+													direction={order}
+												></TableSortLabel>
+											)} */}
+										</TableCell>
+									)
+								)}
+							</TableRow>
+						</TableHead>
+						<TableBody>
+							{userRoles.map(
+								(
+									role: { id: string; name: string; type: string },
+									index: number
+								) => (
+									<TableRow key={role.id}>
+										<TableCell align="left">{index + 1}</TableCell>
+										{keyNames.map((keyName: string, i: number) => {
+											return (
+												<TableCell key={i} align="left">
+													{getValueFromObject(role, keyName.split(","))}
+												</TableCell>
+											);
+										})}
+										{userRoleEditAccess && (
+											<TableCell align="left">
+												<Button
+													variant="contained"
+													size="small"
+													color="primary"
+													component={Link}
+													to="/settings/add_role"
+													state={{ role: role.id }}
+												>
+													Edit Role
+												</Button>
+											</TableCell>
+										)}
+									</TableRow>
+								)
+							)}
+						</TableBody>
+						{
+							// <TableFooter>
+							// 	<TableRow>
+							// 		<TablePagination
+							// 			rowsPerPageOptions={[]}
+							// 			colSpan={8}
+							// 			count={count}
+							// 			rowsPerPage={count > 10 ? 10 : count}
+							// 			page={page}
+							// 			SelectProps={{
+							// 				inputProps: { "aria-label": "rows per page" },
+							// 				native: true,
+							// 			}}
+							// 			onChangePage={(
+							// 				event: React.MouseEvent<HTMLButtonElement> | null,
+							// 				newPage: number
+							// 			) => {
+							// 				if (newPage > page) {
+							// 					changePage();
+							// 				} else {
+							// 					changePage(true);
+							// 				}
+							// 				setPage(newPage);
+							// 			}}
+							// 			onChangeRowsPerPage={() => {}}
+							// 			style={{ paddingRight: "40px" }}
+							// 		/>
+							// 	</TableRow>
+							// </TableFooter>
+						}
+					</Table>
+				</TableContainer>
+			</Grid>
+		</Grid>
+	);
 }
 
 export default RoleTableView;
