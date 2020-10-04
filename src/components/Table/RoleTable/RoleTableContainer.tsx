ecf976eeaeda26536209855c63782fe4b583b43e
shubham <shubham.kumar@dhwaniris.com>
2020-10-04 14:07:28 +0530

    loading screen added to roles table

--- src/components/Table/RoleTable/RoleTableContainer.tsx
+++ src/components/Table/RoleTable/RoleTableContainer.tsx
@@ -5,7 +5,9 @@ import { USER_PERMISSIONS_ACTIONS } from "../../../utils/access/modules/userPerm
 
 function RoleTableContainer({
 	userRoles,
+	loading
 }: {
+	loading: boolean;
 	userRoles: { id: string; name: string; type: string }[];
 }) {
 	const [page, setPage] = useState(0);
@@ -25,6 +27,7 @@ function RoleTableContainer({
 			changePage={(prev?: boolean) => {}}
 			count={0}
 			userRoleEditAccess={userRoleEditAccess}
+			loading={loading}
 		/>
 	);
 }
