f8df5011195e1272435497026a16a9bdb6de7c84
shubham <shubham.kumar@dhwaniris.com>
2020-10-22 04:59:43 +0530

    contact details file setup done

---
+++ src/components/Forms/ContactDetails/Address/AddressGraphql.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import AddressContainer from "./AddressContainer";
+
+function AddressGraphql() {
+	return <AddressContainer />;
+}
+
+export default AddressGraphql;
---
+++ src/models/individual/index.ts
@@ -0,0 +1,4 @@
+export interface IIndividual {
+	first_name: string;
+	last_name: string;
+}
