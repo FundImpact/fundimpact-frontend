9ea6baad37f1b9b6d2c1ecfc56590f31b30f0450
shubham <shubham.kumar@dhwaniris.com>
2020-10-22 06:23:10 +0530

    file import bug removed

--- src/components/Forms/ContactDetails/ContactForm/ContactFormContainer.tsx
+++ src/components/Forms/ContactDetails/ContactForm/ContactFormContainer.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { contactFormFields } from "./inputField.json";
-import { FORM_ACTIONS } from "../../constant.js";
 
 (contactFormFields[5].optionsArray as { id: string; name: string }[]) = [
 	{ id: "1", name: "PERSONAL" },
