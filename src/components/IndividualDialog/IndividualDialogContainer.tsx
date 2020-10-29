de6ae21644b18a217e6620e7d526cf06c9a6717f
shubham <shubham.kumar@dhwaniris.com>
2020-10-29 06:14:57 +0530

    api connected to individual form

--- src/components/IndividualDialog/IndividualDialogContainer.tsx
+++ src/components/IndividualDialog/IndividualDialogContainer.tsx
@@ -67,6 +67,14 @@ interface ISubmitForm {
 	notificationDispatch: React.Dispatch<any>;
 }
 
+interface ISubmitForm {
+	valuesSubmitted: IIndividualForm;
+	createIndividual: (
+		options?: MutationFunctionOptions<ICreateIndividual, ICreateIndividualVariables> | undefined
+	) => Promise<FetchResult<ICreateIndividual, Record<string, any>, Record<string, any>>>;
+	notificationDispatch: React.Dispatch<any>;
+}
+
 const getInitialFormValues = (): IIndividualForm => {
 	return {
 		name: "",
