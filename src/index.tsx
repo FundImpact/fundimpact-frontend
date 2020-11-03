3c278e2152a74364efa961a1a683a0883d98511f
shubham <shubham.kumar@dhwaniris.com>
2020-11-03 12:25:45 +0530

    keys send to config file

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@ import "./index.css";
 
 import React from "react";
 import ReactDOM from "react-dom";
-import { IntlProvider } from "react-intl";
-
 import App from "./App";
 import { UIProvider } from "./contexts/uiContext";
 import { UserProvider } from "./contexts/userContext";
@@ -11,9 +9,10 @@ import * as serviceWorker from "./serviceWorker";
 import { DashboardProvider } from "./contexts/dashboardContext";
 import { MultilingualProvider } from "./contexts/multilingualContext";
 import ReactGA from "react-ga";
+import { keys } from "./config.json";
 
 function initializeReactGa() {
-	ReactGA.initialize("G-0J46Q89H5R");
+	ReactGA.initialize(keys.GOOGLE_ANALYTICS);
 	ReactGA.pageview(window.location.pathname + window.location.search);
 }
 
