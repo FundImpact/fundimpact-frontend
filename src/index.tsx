ec3129b8f05b9e05d83a1249fa56e7546475c5d4
shubham <shubham.kumar@dhwaniris.com>
2020-11-03 11:56:32 +0530

    google analytics added

--- src/index.tsx
+++ src/index.tsx
@@ -10,8 +10,16 @@ import { UserProvider } from "./contexts/userContext";
 import * as serviceWorker from "./serviceWorker";
 import { DashboardProvider } from "./contexts/dashboardContext";
 import { MultilingualProvider } from "./contexts/multilingualContext";
+import ReactGA from "react-ga";
+
+function initializeReactGa() {
+	ReactGA.initialize("G-0J46Q89H5R");
+	ReactGA.pageview(window.location.pathname + window.location.search);
+}
 
 (async function () {
+	initializeReactGa();
+
 	ReactDOM.render(
 		<UserProvider>
 			<MultilingualProvider>
