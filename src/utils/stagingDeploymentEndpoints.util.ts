125bf27680a558b4351890665c767dbabcbfb42c
shubhamsingh3211 <69340232+shubhamsingh3211@users.noreply.github.com>
2021-05-03 18:33:38 +0530

    url changed to https

--- src/utils/stagingDeploymentEndpoints.util.ts
+++ src/utils/stagingDeploymentEndpoints.util.ts
@@ -1,4 +1,4 @@
-export const STAGING = "http://stg.fundimpact.org";
-export const DEMO1 = "http://demo1.fundimpact.org";
-export const DEMO = "http://demo.fundimpact.org";
+export const STAGING = "https://stg.fundimpact.org";
+export const DEMO1 = "https://demo1.fundimpact.org";
+export const DEMO = "https://demo.fundimpact.org";
 export const LOCAL = "http://localhost:3000";
