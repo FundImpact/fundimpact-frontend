01a02795ef99e9d865bf3f5a619a30e620b4bc07
shubhamsingh3211 <69340232+shubhamsingh3211@users.noreply.github.com>
2021-05-03 17:50:00 +0530

    sensitive comment added for url

--- src/utils/stagingDeploymentEndpoints.util.ts
+++ src/utils/stagingDeploymentEndpoints.util.ts
@@ -1,4 +1,4 @@
-export const STAGING = "http://stg.fundimpact.org";
-export const DEMO1 = "http://demo1.fundimpact.org";
-export const DEMO = "http://demo.fundimpact.org";
-export const LOCAL = "http://localhost:3000";
+export const STAGING = "http://stg.fundimpact.org"; // Sensitive
+export const DEMO1 = "http://demo1.fundimpact.org"; // Sensitive
+export const DEMO = "http://demo.fundimpact.org"; // Sensitive
+export const LOCAL = "http://localhost:3000"; // Sensitive
