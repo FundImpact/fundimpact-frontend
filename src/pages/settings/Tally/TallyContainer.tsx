48f7fd3cbcf5ab4c9971408bc3a31e8d740a78f9
Elham13 <elhamuddin.mahmoodi@gmail.com>
2021-10-10 14:09:28 -0700

    Added Tally Forms

--- src/pages/settings/Tally/TallyContainer.tsx
+++ src/pages/settings/Tally/TallyContainer.tsx
@@ -1,42 +1,130 @@
-import React, { useState } from "react";
-import {
-	Box,
-	Button,
-	FormControl,
-	FormControlLabel,
-	FormLabel,
-	RadioGroup,
-	Radio,
-	Dialog,
-	DialogTitle,
-	DialogContent,
-	IconButton,
-} from "@material-ui/core";
-import CloseIcon from "@material-ui/icons/Close";
+import React, { useEffect, useState } from "react";
+import { Box, Button, FormControlLabel, RadioGroup, Radio } from "@material-ui/core";
 import FIDialog from "../../../components/Dialog/Dialog";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import { donorForm, projectForm, targetForm, budgetCategoryForm } from "./inputField.json";
+import Tallyforms from "../../../components/Forms/Tally";
 
 const useStyle = makeStyles((theme: Theme) =>
 	createStyles({
-		ddd: {
-			display: "flex",
-			justifyContent: "space-between",
-			padding: "0px",
-			marginTop: "1px",
+		formWrapper: {
+			minHeight: theme.spacing(40),
 		},
-		title: {
-			backgroundColor: "yellow",
+		childrenWrapper: {
+			width: "50%",
+			marginTop: theme.spacing(5),
+			marginBottom: theme.spacing(2),
 		},
 	})
 );
 
+const getInitialValues = (keyword: string) => {
+	if (keyword === "donor")
+		return {
+			donor: "",
+			project: "",
+			target: "",
+			subTarget: "",
+			projectBudgetCategory: "",
+		};
+
+	if (keyword === "project")
+		return {
+			projects: "",
+			target: "",
+			subTarget: "",
+			projectBudgetCategory: "",
+		};
+
+	if (keyword === "target")
+		return {
+			target: "",
+		};
+
+	if (keyword === "budget_category")
+		return {
+			budget_category: "",
+		};
+};
+
 const TallyContainer = () => {
 	const classes = useStyle();
 	const [openDialog, setOpenDialog] = useState<boolean>(false);
+	const [globalSelect, setGlobalSelect] = useState<string>("donor");
+	const [donorSelect, setDonorSelect] = useState<string>("targetsAndSubTargets");
 
 	const handleCloseDialog = () => {
 		setOpenDialog(false);
 	};
+
+	const handleGlobalSelection = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setGlobalSelect(e.target.value);
+	};
+
+	const handleDonorSelection = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setDonorSelect(e.target.value);
+	};
+
+	const validate = (values: any) => {
+		let errors: Partial<any> = {};
+		// if (props.type === DELIVERABLE_ACTIONS.CREATE) {
+
+		// if (!values.donor) {
+		// 	errors.donor = "Donor is required";
+		// }
+		// if (!values.project) {
+		// 	errors.project = "Projcet is required";
+		// }
+		// // }
+
+		// if (props.type === DELIVERABLE_ACTIONS.UPDATE) {
+		// 	if (!values.name && !values.name.length) {
+		// 		errors.name = "Name is required";
+		// 	}
+		// 	if (!values.project) {
+		// 		errors.project = "Project is required";
+		// 	}
+		// 	if (values.is_qualitative) {
+		// 		if (!values.value_qualitative_option) {
+		// 			errors.value_qualitative_option = "Options are required";
+		// 		}
+		// 	} else {
+		// 		if (!values.value_calculation) {
+		// 			errors.value_calculation = "This Field is required";
+		// 		}
+		// 	}
+		// }
+		return errors;
+	};
+
+	const onCreate = (value: any) => {
+		console.log("Oncreate: ", value);
+	};
+	const onUpdate = (value: any) => {
+		console.log("Onupdate: ", value);
+	};
+
+	const onCancel = () => {
+		handleCloseDialog();
+	};
+
+	if (donorSelect === "targetsAndSubTargets") {
+		donorForm[2].hidden = false;
+		donorForm[3].hidden = false;
+		donorForm[4].hidden = true;
+		projectForm[1].hidden = false;
+		projectForm[2].hidden = false;
+		projectForm[3].hidden = true;
+	}
+
+	if (donorSelect === "projectBudgetCategory") {
+		donorForm[2].hidden = true;
+		donorForm[3].hidden = true;
+		donorForm[4].hidden = false;
+		projectForm[1].hidden = true;
+		projectForm[2].hidden = true;
+		projectForm[3].hidden = false;
+	}
 
 	return (
 		<Box p={2}>
@@ -45,22 +133,14 @@ const TallyContainer = () => {
 				Open Dialogue
 			</Button>
 
-			<Dialog open={openDialog} onClose={handleCloseDialog} fullWidth maxWidth="md">
-				<Box className={classes.ddd}>
-					<DialogTitle>Title</DialogTitle>
-					<IconButton
-						aria-label="close"
-						onClick={handleCloseDialog}
-						style={{ backgroundColor: "transparent" }}
-					>
-						<CloseIcon />
-					</IconButton>
-				</Box>
-				<DialogContent>hi</DialogContent>
-			</Dialog>
-			{/* <FIDialog open={openDialog} handleClose={handleCloseDialog} header="Cost Center Name">
-				<FormControl component="fieldset">
-					<RadioGroup row aria-label="tally" name="row-radio-buttons-group">
+			<FIDialog open={openDialog} handleClose={handleCloseDialog} header="Cost Center Name">
+				<RadioGroup
+					aria-label="tally"
+					name="row-radio-buttons-group"
+					onChange={handleGlobalSelection}
+					value={globalSelect}
+				>
+					<Box display="flex" justifyContent="space-between">
 						<FormControlLabel value="donor" control={<Radio />} label="Donor" />
 						<FormControlLabel value="project" control={<Radio />} label="Project" />
 						<FormControlLabel value="target" control={<Radio />} label="Target" />
@@ -69,9 +149,111 @@ const TallyContainer = () => {
 							control={<Radio />}
 							label="Budget Category"
 						/>
-					</RadioGroup>
-				</FormControl>
-			</FIDialog> */}
+					</Box>
+				</RadioGroup>
+
+				<Box className={classes.formWrapper} mt={5}>
+					{globalSelect === "donor" && (
+						<Tallyforms
+							{...{
+								initialValues: getInitialValues("donor"),
+								inputFields: donorForm,
+								validate,
+								onSubmit: onCreate,
+								onCancel,
+								formAction: undefined,
+								onUpdate,
+							}}
+						>
+							<Box className={classes.childrenWrapper}>
+								<RadioGroup
+									aria-label="tally"
+									name="row-radio-buttons-group"
+									onChange={handleDonorSelection}
+									value={donorSelect}
+								>
+									<Box display="flex" justifyContent="space-between">
+										<FormControlLabel
+											value="targetsAndSubTargets"
+											control={<Radio />}
+											label="Targets and Sub-Targets"
+										/>
+										<FormControlLabel
+											value="projectBudgetCategory"
+											control={<Radio />}
+											label="Project Budget Category"
+										/>
+									</Box>
+								</RadioGroup>
+							</Box>
+						</Tallyforms>
+					)}
+
+					{globalSelect === "project" && (
+						<Tallyforms
+							{...{
+								initialValues: getInitialValues("project"),
+								inputFields: projectForm,
+								validate,
+								onSubmit: onCreate,
+								onCancel,
+								formAction: undefined,
+								onUpdate,
+							}}
+						>
+							<Box className={classes.childrenWrapper}>
+								<RadioGroup
+									aria-label="tally"
+									name="row-radio-buttons-group"
+									onChange={handleDonorSelection}
+									value={donorSelect}
+								>
+									<Box display="flex" justifyContent="space-between">
+										<FormControlLabel
+											value="targetsAndSubTargets"
+											control={<Radio />}
+											label="Targets and Sub-Targets"
+										/>
+										<FormControlLabel
+											value="projectBudgetCategory"
+											control={<Radio />}
+											label="Project Budget Category"
+										/>
+									</Box>
+								</RadioGroup>
+							</Box>
+						</Tallyforms>
+					)}
+
+					{globalSelect === "target" && (
+						<Tallyforms
+							{...{
+								initialValues: getInitialValues("target"),
+								inputFields: targetForm,
+								validate,
+								onSubmit: onCreate,
+								onCancel,
+								formAction: undefined,
+								onUpdate,
+							}}
+						/>
+					)}
+
+					{globalSelect === "budget_category" && (
+						<Tallyforms
+							{...{
+								initialValues: getInitialValues("budget_category"),
+								inputFields: budgetCategoryForm,
+								validate,
+								onSubmit: onCreate,
+								onCancel,
+								formAction: undefined,
+								onUpdate,
+							}}
+						/>
+					)}
+				</Box>
+			</FIDialog>
 		</Box>
 	);
 };
