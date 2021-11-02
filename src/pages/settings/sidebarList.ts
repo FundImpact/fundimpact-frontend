59b1905b2a2d37578a18b86d0daf6056c431cf6d
abdul_qadir_dhwani <abdul.qadir@dhwaniris.com>
2021-11-02 11:23:42 +0530

    :lipstick: fix auto managemaster

--- src/pages/settings/sidebarList.ts
+++ src/pages/settings/sidebarList.ts
@@ -1,47 +1,18 @@
-const manageMasterSubHeadings = [
-	{
-		to: "budget",
-		dataTestId: "budget-category-link",
-		title: "Budget Categories",
-		userAccess: true,
-	},
-	{
-		to: "categories",
-		dataTestId: "categories-link",
-		title: "Categories",
-		userAccess: true,
-	},
-	{
-		to: "units",
-		dataTestId: "units-link",
-		title: "Units",
-		userAccess: true,
-	},
-	{
-		to: "yeartags",
-		dataTestId: "yeartags-link",
-		title: "Year Tags",
-		userAccess: true,
-	},
-	{
-		to: "tally",
-		dataTestId: "tally-link",
-		title: "Tally Masters",
-		userAccess: true,
-	},
-	{
-		to: "goeGraphies",
-		dataTestId: "goeGraphies-link",
-		title: "Geographies",
-		userAccess: true,
-	},
-	{
-		to: "goeRegions",
-		dataTestId: "geoRegions-link",
-		title: "GeoRegions",
-		userAccess: true,
-	},
+const manageMasterObject = [
+	"budget",
+	"categories",
+	"units",
+	"yeartags",
+	"tally",
+	"goeGraphies",
+	"goeRegions",
 ];
+const manageMasterSubHeadings = manageMasterObject.map((item) => ({
+	to: item,
+	dataTestId: item == "budget" ? "budget-category-link" : item + "-link",
+	title: item.charAt(0).toUpperCase() + item.slice(1),
+	userAccess: true,
+}));
 export const sidebarList: {
 	mainHeading: string;
 	subHeadings: { to: string; dataTestId: string; title: string; userAccess: boolean }[];
